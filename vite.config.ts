6429c4d2e3a05b587b998a46f86543677e057de7
forouz71@gmail.com <forouz71@gmail.com>
2026-08-10 01:14:46 +0330

    chore: add Storybook setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,47 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 import { fileURLToPath } from "url"
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin"
+import { playwright } from "@vitest/browser-playwright"
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(dirname, "./src"),
     },
+  },
+
+  test: {
+    projects: [
+      {
+        extends: true,
+
+        plugins: [
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+
+        test: {
+          name: "storybook",
+
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+      },
+    ],
   },
 })
